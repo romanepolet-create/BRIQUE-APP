62978bcdcc3363f35463d952f5aa3b6e61917d0c
romanepolet-create <romane.polet@briquehouse.fr>
2026-05-25 14:30:55 +0200

    Update distrib.js

--- routes/distrib.js
+++ routes/distrib.js
@@ -9,14 +9,23 @@ const supabase = createClient('https://mtvzhpqticnxqcxsadkg.supabase.co', 'eyJhb
 // On stocke le chemin vers le fichier pour ne pas le retaper partout
 const cheminDistrib = path.join(__dirname, '../data', 'distrib.json');
 
+
 router.get('/', async (req, res) => {
   try {
-    const data = await fs.readFile(cheminDistrib, 'utf-8');
-    res.json(JSON.parse(data));
+    // On demande TOUTES les données de la table 'distributeurs' à Supabase
+    const { data, error } = await supabase.from('distributeurs').select('*');
+    
+    if (error) {
+      throw error;
+    }
+    
+    // On renvoie la vraie donnée fraîche au frontend
+    res.json(data);
+    
   } catch (erreur) {
-   console.error("Erreur de lecture de distrib.json :", erreur);
-   res.status(500).json({ message: "Impossible de charger les distributeurs" });
- }
+    console.error("Erreur de lecture Supabase :", erreur);
+    res.status(500).json({ message: "Impossible de charger les distributeurs" });
+  }
 });
 
 // ==========================================
