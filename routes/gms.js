2d7e850ad581c953cb753f66360b3f4cb36bda9c
romanepolet-create <romane.polet@briquehouse.fr>
2026-06-10 16:06:59 +0200

    Update gms.js

--- routes/gms.js
+++ routes/gms.js
@@ -6,7 +6,7 @@ const path = require('path');
 const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient('https://mtvzhpqticnxqcxsadkg.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10dnpocHF0aWNueHFjeHNhZGtnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3ODAyOTc1MCwiZXhwIjoyMDkzNjA1NzUwfQ.r2JXBf8hLrukjKOLQFlVqCBXDx8AjxlgVMGKir1a55U');
 
-router.get('/', async (req, res) => {
+/*router.get('/', async (req, res) => {
   try {
     // On demande à Supabase de récupérer toutes les lignes de la table GMS
     const { data, error } = await supabase
@@ -26,6 +26,21 @@ router.get('/', async (req, res) => {
   } catch (err) {
     console.error("Erreur Supabase GMS :", err);
     res.status(500).json({ error: "Erreur lors de la récupération des magasins." });
+  }
+});
+
+module.exports = router;*/
+
+const cheminMapGMS = path.join(__dirname, '../data', 'mapGMS.json');
+
+router.get('/', async (req, res) => {
+	try { 
+		const data = await fs.readFile(cheminMapGMS, 'utf-8');
+		
+    res.json(JSON.parse(data));
+  } catch(erreur) {
+    console.error('Erreur de lecture de proforma.json :', erreur);
+    res.status(500).json({ message: "Impossible de charger les donnees magasins" });
   }
 });
 
