3dbc50d02b1dc8347183c9d6d7376807695aeef0
romanepolet-create <romane.polet@briquehouse.fr>
2026-09-09 16:15:41 +0200

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -13,7 +13,7 @@ router.get('/data', async (req, res) => {
         const now = new Date();
         const firstDayLastMonth = new Date(now.getFullYear(), now.getMonth() - 1, 1).toISOString();
 
-        const { data: visitesData } = await supabase.from('dashboard_visites').select('*');
+        const { data: visitesData } = await supabase.from('dashboard_visites').select('*').limit(1000000000000);
         const visitesBrutes = Array.isArray(visitesData) ? visitesData : [];
 
         const { data: objData } = await supabase.from('objectifs_commerciaux').select('*');
