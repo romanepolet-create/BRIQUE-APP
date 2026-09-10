097acca86870c6c6190939c95ed113b8f8dcdc26
romanepolet-create <romane.polet@briquehouse.fr>
2026-09-10 16:03:06 +0200

    Update dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -389,7 +389,7 @@ function genererFocusMEA(visites) {
         const dateFormatee = new Date(v.created_at).toLocaleDateString('fr-FR');
         
         const magInfo = donneesGlobales.listeMagasins.find(m => m.hubspot_id === v.hubspot_id);
-        const nomMagasin = magInfo ? magInfo.nom : v.hubspot_id;
+        const nomMagasin = v.nom ? v.nom : v.hubspot_id;
 
         const tr = document.createElement('tr');
         tr.innerHTML = `
